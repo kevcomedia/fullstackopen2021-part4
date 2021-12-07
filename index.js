ef7942a9efa0e319bfd22d728c6bccd6d7872e2e
Kev Comedia <kevcomedia@gmail.com>
2021-12-07 17:26:25 +0800

    Move express app to its own file

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const config = require('./utils/config')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
 const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
 
 const server = http.createServer(app)
 server.listen(config.PORT, () => {
