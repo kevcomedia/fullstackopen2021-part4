a2d67088ae94fe5ebc59e8597ba7d7e445da9a66
Kev Comedia <kevcomedia@gmail.com>
2021-12-07 17:40:26 +0800

    Add middleware for handling unknown endpoints

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -19,5 +20,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
 
 module.exports = app
