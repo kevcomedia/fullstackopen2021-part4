6bd8ad22f3998509ea8be5bd252e5efe1ea8b1ac
Kev Comedia <kevcomedia@gmail.com>
2021-12-07 17:36:05 +0800

    Move connection code to app.js
    It was supposed to be in app.js, not in index.js.

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Cannot connect to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
--- index.js
+++ index.js
@@ -2,17 +2,6 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const http = require('http')
 const app = require('./app')
-const mongoose = require('mongoose')
-
-const mongoUrl = config.MONGODB_URI
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    logger.info('Connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('Cannot connect to MongoDB:', error.message)
-  })
 
 const server = http.createServer(app)
 server.listen(config.PORT, () => {
