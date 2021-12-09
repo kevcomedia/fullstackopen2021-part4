5021a18a7831f84f264b71203214df1f3ff4c81d
Kev Comedia <kevcomedia@gmail.com>
2021-12-09 18:10:36 +0800

    Populate test db with data before tests

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -1,8 +1,17 @@
 const supertest = require('supertest')
 const mongoose = require('mongoose')
+const testHelper = require('./test_helper')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const initialBlogs = testHelper.blogs
+  const promiseArray = initialBlogs.map((blog) => new Blog(blog).save())
+  await Promise.all(promiseArray)
+}, 10000)
 
 test('blogs are returned as json', async () => {
   await api
