b657476339e291a81106a7dd2249f61a93dd156b
Kev Comedia <kevcomedia@gmail.com>
2021-12-09 18:15:05 +0800

    Write test for GET /api/blogs

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -6,18 +6,20 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
+const initialBlogs = testHelper.blogs
+
 beforeEach(async () => {
   await Blog.deleteMany({})
-  const initialBlogs = testHelper.blogs
   const promiseArray = initialBlogs.map((blog) => new Blog(blog).save())
   await Promise.all(promiseArray)
 }, 20000)
 
 test('blogs are returned as json', async () => {
-  await api
+  const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+  expect(response.body).toHaveLength(initialBlogs.length)
 })
 
 afterAll(() => {
