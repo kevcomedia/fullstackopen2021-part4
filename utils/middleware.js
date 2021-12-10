da24fcb02313398da1c47760731ab719e3d1d397
Kev Comedia <kevcomedia@gmail.com>
2021-12-10 16:01:28 +0800

    Write error handler middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -8,8 +8,17 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const errorHandler = (error, request, response, next) => {
+  switch (error.name) {
+    case 'ValidationError':
+      return response.status(400).send({ error: error.message })
+  }
+
+  next(error)
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-module.exports = { requestLogger, unknownEndpoint }
+module.exports = { requestLogger, errorHandler, unknownEndpoint }
