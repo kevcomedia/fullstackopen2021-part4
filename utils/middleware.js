440d954546c8d1dfdf659c18d9aa9d0fa53e1634
Kev Comedia <kevcomedia@gmail.com>
2021-12-15 17:29:10 +0800

    Add error handling for invalid id

--- utils/middleware.js
+++ utils/middleware.js
@@ -10,6 +10,8 @@ const requestLogger = (request, response, next) => {
 
 const errorHandler = (error, request, response, next) => {
   switch (error.name) {
+    case 'CastError':
+      return response.status(400).send({ error: 'malformatted id' })
     case 'ValidationError':
       return response.status(400).send({ error: error.message })
   }
