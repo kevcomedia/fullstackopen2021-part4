5f588ff5f121a400573c6ed7937097da6134bd89
Kev Comedia <kevcomedia@gmail.com>
2022-01-04 18:55:29 +0800

    Validate unique usernames

--- models/user.js
+++ models/user.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     minLength: 3,
+    unique: true,
   },
   passwordHash: String,
   name: String,
@@ -17,5 +19,7 @@ userSchema.set('toJSON', {
     delete returnedObject.__v
   },
 })
+
+userSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('User', userSchema)
