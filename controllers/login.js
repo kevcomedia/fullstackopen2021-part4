937ed50c05831de907d4237ee7aea3c224298687
Kev Comedia <kevcomedia@gmail.com>
2022-01-13 18:38:02 +0800

    Implement login

---
+++ controllers/login.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response, next) => {
+  const { username, password } = request.body
+
+  try {
+    const user = await User.findOne({ username })
+
+    const passwordCorrect =
+      !user || password == null
+        ? false
+        : await bcrypt.compare(password, user.passwordHash)
+    if (!user || !passwordCorrect) {
+      return response
+        .status(401)
+        .json({ error: 'invalid username or password' })
+    }
+
+    const userForToken = {
+      username,
+      id: user._id,
+    }
+    const token = jwt.sign(userForToken, process.env.JWT_SECRET)
+
+    response.status(200).send({ token, username, name: user.name })
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = loginRouter
