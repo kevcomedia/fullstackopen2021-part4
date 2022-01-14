2ed9f500d32b829eb47e32c85e4f3a00ba4d5545
Kev Comedia <kevcomedia@gmail.com>
2022-01-14 15:34:16 +0800

    Add error-handling for invalid tokens

--- utils/middleware.js
+++ utils/middleware.js
@@ -14,6 +14,8 @@ const errorHandler = (error, request, response, next) => {
       return response.status(400).send({ error: 'malformatted id' })
     case 'ValidationError':
       return response.status(400).send({ error: error.message })
+    case 'JsonWebTokenError':
+      return response.status(401).send({ error: 'token missing or invalid' })
   }
 
   next(error)
